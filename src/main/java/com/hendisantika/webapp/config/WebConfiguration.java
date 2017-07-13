63ea37ea50561c8f7e368b154cc16fac14d96363
Hendi Santika <hendisantika@gmail.com>
2017-07-13 07:59:45 +0700

    #8 - Added Product Model

---
+++ src/main/java/com/hendisantika/webapp/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.hendisantika.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by hendisantika on 7/13/17.
+ */
+@Configuration
+public class WebConfiguration {
+//    @Bean
+//    ServletRegistrationBean h2servletRegistration(){
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+//        registrationBean.addUrlMappings("/console/*");
+//        return registrationBean;
+//    }
+}
