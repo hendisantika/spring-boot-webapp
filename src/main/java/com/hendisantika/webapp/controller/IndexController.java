ff64e5822dc56af4a34b990d2b5eb75ed8bf96f7
Hendi Santika <hendisantika@gmail.com>
2017-07-14 13:08:07 +0700

    #15 - Added Product Controller

--- src/main/java/com/hendisantika/webapp/controller/IndexController.java
+++ src/main/java/com/hendisantika/webapp/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.hendisantika.webapp.controller;
 
-import com.hendisantika.webapp.domain.Product;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by hendisantika on 7/13/17.
@@ -15,18 +12,5 @@ public class IndexController {
     String index(){
         return "index";
     }
-
-    @RequestMapping("product/new")
-    public String newProduct(Model model){
-        model.addAttribute("product", new Product());
-        return "productform";
-    }
-
-    @RequestMapping(value = "product", method = RequestMethod.POST)
-    public String saveProduct(Product product){
-        productService.saveProduct(product);
-        return "redirect:/product/" + product.getId();
-    }   
-
 
 }
