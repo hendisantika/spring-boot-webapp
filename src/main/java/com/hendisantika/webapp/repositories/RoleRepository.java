4345d7170fb4fddc986875aa6f49bdacaca375a5
Hendi Santika <hendisantika@gmail.com>
2017-07-20 06:57:49 +0700

    #38 - Added Role Repository

---
+++ src/main/java/com/hendisantika/webapp/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.hendisantika.webapp.repositories;
+
+import com.hendisantika.webapp.domain.Role;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by hendisantika on 7/20/17.
+ */
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
