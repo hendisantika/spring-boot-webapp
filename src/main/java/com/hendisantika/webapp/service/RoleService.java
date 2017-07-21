5385b98cba0bde78574aadaf7e162b2017c07b81
Hendi Santika <hendisantika@gmail.com>
2017-07-21 14:02:43 +0700

    #41 - Added ROLE Service

---
+++ src/main/java/com/hendisantika/webapp/service/RoleService.java
@@ -0,0 +1,9 @@
+package com.hendisantika.webapp.service;
+
+import com.hendisantika.webapp.domain.Role;
+
+/**
+ * Created by hendisantika on 7/21/17.
+ */
+public interface RoleService extends CRUDService<Role> {
+}
