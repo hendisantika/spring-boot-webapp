ff7d24369377cca6d346e99d1ae4ae7140925310
Hendi Santika <hendisantika@gmail.com>
2017-07-24 05:25:07 +0700

    #50 - Added User Details Service Imp

---
+++ src/main/java/com/hendisantika/webapp/service/security/UserDetailsServiceImp.java
@@ -0,0 +1,43 @@
+package com.hendisantika.webapp.service.security;
+
+import com.hendisantika.webapp.domain.User;
+import com.hendisantika.webapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/24/17
+ * Time: 5:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service("userDetailsService")
+public class UserDetailsServiceImp  implements UserDetailsService {
+    private UserService userService;
+    private Converter<User, UserDetails> userUserDetailsConverter;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    @Qualifier(value = "userToUserDetails")
+    public void setUserUserDetailsConverter(Converter<User, UserDetails> userUserDetailsConverter) {
+        this.userUserDetailsConverter = userUserDetailsConverter;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userUserDetailsConverter.convert(userService.findByUsername(username));
+    }
+}
