146d4d817caf335b69dfbef40a2d3b067d9bfcea
Hendi Santika <hendisantika@gmail.com>
2017-07-23 20:35:03 +0700

    #48 - Added User Details Impl

---
+++ src/main/java/com/hendisantika/webapp/service/security/UserDetailsImpl.java
@@ -0,0 +1,75 @@
+package com.hendisantika.webapp.service.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/23/17
+ * Time: 8:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserDetailsImpl implements UserDetails {
+    private Collection<SimpleGrantedAuthority> authorities;
+    private String username;
+    private String password;
+    private Boolean enabled = true;
+
+    public void setAuthorities(Collection<SimpleGrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+}
