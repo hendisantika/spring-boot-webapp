bfc046333dc0cc0f04109e8f94e41141d5937711
Hendi Santika <hendisantika@gmail.com>
2017-07-23 20:31:21 +0700

    #46 - Added Encryption Service

---
+++ src/main/java/com/hendisantika/webapp/service/EncryptionService.java
@@ -0,0 +1,15 @@
+package com.hendisantika.webapp.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/23/17
+ * Time: 8:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface EncryptionService {
+    String encryptString(String input);
+    boolean checkPassword(String plainPassword, String encryptedPassword);
+}
