d366795d29a36048e018e71604f08fc04179d97d
Hendi Santika <hendisantika@gmail.com>
2017-07-26 06:16:45 +0700

    #57 - Added RoleServiceMapImpl

---
+++ src/main/java/com/hendisantika/webapp/service/mapservices/RoleServiceMapImpl.java
@@ -0,0 +1,19 @@
+package com.hendisantika.webapp.service.mapservices;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/26/17
+ * Time: 6:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+@Profile("map")
+public class RoleServiceMapImpl extends AbstractMapService{
+}
